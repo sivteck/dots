9ef500814202e524017dd8d4e10bbbf1a6e3757a
Sivaram Balakrishnan <sivaram.balakrishnan@quintype.com>
2022-03-04 09:35:42 +0530

    add Esc for terminal

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,2 +1,10 @@
 vim.api.nvim_set_keymap('i', 'df', '<Esc>', { noremap = true })
 vim.g.mapleader = ","
+
+vim.api.nvim_exec(
+[[
+if has("nvim")
+  au TermOpen * tnoremap <Esc> <c-\><c-n>
+endif
+]],
+false)
