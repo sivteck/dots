1620fd20dc05572ad45647c5423da5693a22e0ee
Sivaram Balakrishnan <sivaram@iyangi.com>
2022-07-28 13:08:19 +0530

    add keymap to jump to prev buffer

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_set_keymap('i', 'df', '<Esc>', { noremap = true })
 vim.g.mapleader = " "
+vim.api.nvim_set_keymap('n', '<Leader>b', ':b#<CR>', { noremap = true })
 
 
 vim.api.nvim_exec(
